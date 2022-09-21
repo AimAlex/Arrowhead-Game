a48f35179fc349e10167d32ca67eba8bb8a846d8
heyilin19 <heyilin@usc.edu>
2022-09-21 08:55:30 -0700

    Scene level1
    show hint1 hint2 hint3

---
+++ Assets/Scripts/Level1_hint.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1_hint : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject hint1_panel;
+    public GameObject hint2_panel;
+    public GameObject hint3_panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hint1_panel.SetActive(false);
+        hint2_panel.SetActive(false);
+        hint3_panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public void showhint()
+    {
+        float player_x = player.transform.position.x;
+        if(player_x > -19 && player_x < 28)
+        {
+            // hint1_panel = GameObject.Find("Canvas/Hint1_panel");
+            hint1_panel.SetActive(true);
+            // hint2_panel.SetActive(false);
+            // hint3_panel.SetActive(false);
+        }
+        else if(player_x > 28 && player_x < 86)
+        {
+            // hint1_panel.SetActive(false);
+            hint2_panel.SetActive(true);
+            // hint3_panel.SetActive(false);
+        }
+        else
+        {
+            hint3_panel.SetActive(true);
+        }
+    }
+
+    public void closehint()
+    {
+        float player_x = player.transform.position.x;
+        if(player_x > -19 && player_x < 25)
+        {
+            // hint1_panel = GameObject.Find("Canvas/Hint1_panel");
+            hint1_panel.SetActive(false);
+            // hint2_panel.SetActive(false);
+            // hint3_panel.SetActive(false);
+        }
+        else if(player_x > 32 && player_x < 84)
+        {
+            // hint1_panel.SetActive(false);
+            hint2_panel.SetActive(false);
+            // hint3_panel.SetActive(false);
+        }
+        else
+        {
+            // hint1_panel.SetActive(false);
+            // hint2_panel.SetActive(false);
+            hint3_panel.SetActive(false);
+        }
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,6 +25,10 @@ public class PlayerMovement : MonoBehaviour
 
 	private void OnTriggerEnter2D(Collider2D col)
 	{
+		// if (col.name == "doubleJumpItem")
+		// {
+		// 	collectDoubleJump = true;
+		// }
 		if(col.CompareTag("Booster"))
 		{
 			if (col.name == "doubleJumpItem")
