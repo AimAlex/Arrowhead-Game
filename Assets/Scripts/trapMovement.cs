a8b3ff61a1716244cf8cf169a47586e86faa46db
Jiangbei Li <jiangbei@Jiangbeis-MBP.lan>
2022-09-21 00:59:10 -0700

    modify trap movement

--- Assets/Scripts/trapMovement.cs
+++ Assets/Scripts/trapMovement.cs
@@ -6,28 +6,50 @@ public class trapMovement : MonoBehaviour
 {
 
 
-    [SerializeField]private float upLimit;
-    [SerializeField] private float downLimit;
-    private float speed = 2f;
-    private int direction = 1;
-
+    [SerializeField] private float vertical, horizontal;
+    [SerializeField] private float speed = 2f;
+    private Vector2 startPosition;
+    private PolygonCollider2D pc;
+    private int verticalDir = 0;
+    private int horizontalDir = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (vertical != 0)
+        {
+            verticalDir = 1;
+        }
+
+        if (horizontal != 0)
+        {
+            horizontalDir = 1;
+        }
+        pc = GetComponent<PolygonCollider2D>();
+        startPosition = pc.transform.position;
     }
 
     // Update is called once per frame
  
-    void Update () {
-        if (transform.position.y > upLimit) {
-            direction = -1;
+    void Update ()
+    {
+        if (vertical > 0 && transform.position.y - startPosition.y > vertical) {
+            verticalDir = -1;
         }
-        else if (transform.position.y < downLimit) {
-            direction = 1;
+        else if (vertical > 0 && startPosition.y - transform.position.y > vertical) {
+            verticalDir = 1;
         }
-        // movement = 
-        transform.Translate(Vector3.up * direction * speed * Time.deltaTime); 
-  }
+
+        if (horizontal > 0 && transform.position.x - startPosition.x > horizontal)
+        {
+            horizontalDir = -1;
+        }
+        else if (horizontal > 0 && startPosition.x - transform.position.x > horizontal)
+        {
+            horizontalDir = 1;
+        }
+        
+        transform.Translate(verticalDir * speed * Time.deltaTime * Vector3.up);
+        transform.Translate(horizontalDir * speed * Time.deltaTime * Vector3.right);
+    }
 }
