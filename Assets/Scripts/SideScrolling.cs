46b66947fbd8626e729317eb06fc993c9e298f6c
Jiangbei Li <jiangbei@Jiangbeis-MBP.lan>
2022-09-21 23:07:40 -0700

    better camera

--- Assets/Scripts/SideScrolling.cs
+++ Assets/Scripts/SideScrolling.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class SideScrolling : MonoBehaviour
 {
     private Transform player;
+    private Vector3 startPosition;
+    [SerializeField] private float smoothing;
 
     private void Awake()
     {
         player = GameObject.Find("Player").transform;
+        startPosition = transform.position - player.position;
     }
 
     private void LateUpdate()
     {
         Vector3 cameraPosition = transform.position;
         cameraPosition.x = player.position.x;
-        cameraPosition.y = player.position.y;
-        transform.position = cameraPosition;
+        cameraPosition.y = player.position.y + startPosition.y;
+        if (cameraPosition != transform.position)
+        {
+            transform.position = cameraPosition;
+        }
+        
     }
 
 }
