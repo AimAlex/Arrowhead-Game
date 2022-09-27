963f9d7111164f15d70d861b1875df7af6ddca9b
sqz0914 <sqz0914@gmail.com>
2022-09-26 21:34:32 -0700

    Qizhuo: Add Shoot (Incomplete)

---
+++ Assets/Weapon.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public itemCollect items;
+    public Transform firePoint;
+    // public GameObject bulletPrefab;
+    public LineRenderer lineRenderer;
+    public Vector2 direction;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePos - (Vector2)firePoint.position;
+        if(Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, direction);
+        if(hitInfo)
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+            if(hitInfo.transform.tag == "Treasure")
+            {
+                Collider2D col = hitInfo.collider;
+                GameObject item = col.gameObject;
+                // TO DO: Figure out how to get grid y position
+                item.transform.position = new Vector3(item.transform.position.x, -7.8223f, item.transform.position.z);
+            }
+
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);           
+        }
+
+        lineRenderer.enabled = true;
+        yield return new WaitForSeconds(0.02f);
+        lineRenderer.enabled = false;
+    }
+}
