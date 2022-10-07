7e7539c7d6a24b9833ffa93fc60ebd186635689a
Xintongch <50897973+xintongch@users.noreply.github.com>
2022-10-07 04:17:06 -0700

    Revert "Revert "update laserGun script""
    This reverts commit 1d556e0cc2f522218f852a291f500fc1b0beafb5.

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -11,6 +11,8 @@ public class Weapon : MonoBehaviour
     public static bool laserGunPickUp;
     public Vector2 direction;
     // public GameObject ground;
+    Vector2 mousePos;
+    RaycastHit2D hitInfo;
 
     private void Awake()
     {
@@ -20,7 +22,7 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         direction = mousePos - (Vector2)firePoint.position;
         if(Input.GetKeyDown(KeyCode.F) && laserGunPickUp == true)
         {
@@ -30,83 +32,97 @@ public class Weapon : MonoBehaviour
 
     IEnumerator Shoot()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, direction);
-        if(hitInfo && hitInfo.transform.tag != "Finish")
-        {
+        // modified codes
+        RaycastHit2D[] hitInfos = Physics2D.RaycastAll(firePoint.position, direction);
+        if(hitInfos.Length==0 || (hitInfos.Length==1 && hitInfos[0].transform.tag=="Player")){
             lineRenderer.SetPosition(0, firePoint.position);
-            lineRenderer.SetPosition(1, hitInfo.point);
-            Collider2D col = hitInfo.collider;
-            GameObject item = col.gameObject;
-            if(hitInfo.transform.tag == "Treasure")
+            lineRenderer.SetPosition(1, mousePos*100); 
+        }else{
+
+            if(hitInfos[0].transform.tag=="Player"){
+                hitInfo=hitInfos[1];
+            }else{
+                hitInfo=hitInfos[0];
+            }
+        
+
+
+            if(hitInfo && hitInfo.transform.tag != "Finish")
             {
-                // item.transform.position = new Vector3(item.transform.position.x, ground.transform.position.y + 1.0f, item.transform.position.z);
-                if(itemCollect.bagStack.Count < 3)
+                lineRenderer.SetPosition(0, firePoint.position);
+                lineRenderer.SetPosition(1, hitInfo.point);
+                Collider2D col = hitInfo.collider;
+                GameObject item = col.gameObject;
+                if(hitInfo.transform.tag == "Treasure")
                 {
-                    itemCollect.itemNumber = itemCollect.itemNumber + 1;
-                    itemCollect.onPickObject = item;
-                    itemCollect.onPickObject.SetActive(false);
-                    itemCollect.bagStack.Push(itemCollect.onPickObject);
-                    if (itemCollect.tool1.sprite == null)
+                    // item.transform.position = new Vector3(item.transform.position.x, ground.transform.position.y + 1.0f, item.transform.position.z);
+                    if(itemCollect.bagStack.Count < 3)
                     {
-                        itemCollect.tool1.sprite = itemCollect.onPickObject.GetComponent<SpriteRenderer>().sprite;
-                        itemCollect.tool1.color = itemCollect.onPickObject.GetComponent<SpriteRenderer>().color;
-                        if (itemCollect.tool1.sprite == itemCollect.tool4.sprite) {
-                            itemCollect.tool4.sprite = null;
-                            itemCollect.tool4.color = Color.clear;
-                        } else if (itemCollect.tool1.sprite == itemCollect.tool5.sprite) {
-                            itemCollect.tool5.sprite = null;
-                            itemCollect.tool5.color = Color.clear;
-                        } else if (itemCollect.tool1.sprite == itemCollect.tool6.sprite) {
-                            itemCollect.tool6.sprite = null;
-                            itemCollect.tool6.color = Color.clear;
+                        itemCollect.itemNumber = itemCollect.itemNumber + 1;
+                        itemCollect.onPickObject = item;
+                        itemCollect.onPickObject.SetActive(false);
+                        itemCollect.bagStack.Push(itemCollect.onPickObject);
+                        if (itemCollect.tool1.sprite == null)
+                        {
+                            itemCollect.tool1.sprite = itemCollect.onPickObject.GetComponent<SpriteRenderer>().sprite;
+                            itemCollect.tool1.color = itemCollect.onPickObject.GetComponent<SpriteRenderer>().color;
+                            if (itemCollect.tool1.sprite == itemCollect.tool4.sprite) {
+                                itemCollect.tool4.sprite = null;
+                                itemCollect.tool4.color = Color.clear;
+                            } else if (itemCollect.tool1.sprite == itemCollect.tool5.sprite) {
+                                itemCollect.tool5.sprite = null;
+                                itemCollect.tool5.color = Color.clear;
+                            } else if (itemCollect.tool1.sprite == itemCollect.tool6.sprite) {
+                                itemCollect.tool6.sprite = null;
+                                itemCollect.tool6.color = Color.clear;
+                            }
                         }
-                    }
-                    else if (itemCollect.tool2.sprite == null)
-                    {
-                        itemCollect.tool2.sprite = itemCollect.onPickObject.GetComponent<SpriteRenderer>().sprite;
-                        itemCollect.tool2.color = itemCollect.onPickObject.GetComponent<SpriteRenderer>().color;
-                        if (itemCollect.tool2.sprite == itemCollect.tool4.sprite) {
-                            itemCollect.tool4.sprite = null;
-                            itemCollect.tool4.color = Color.clear;
-                        } else if (itemCollect.tool2.sprite == itemCollect.tool5.sprite) {
-                            itemCollect.tool5.sprite = null;
-                            itemCollect.tool5.color = Color.clear;
-                        } else if (itemCollect.tool2.sprite == itemCollect.tool6.sprite) {
-                            itemCollect.tool6.sprite = null;
-                            itemCollect.tool6.color = Color.clear;
+                        else if (itemCollect.tool2.sprite == null)
+                        {
+                            itemCollect.tool2.sprite = itemCollect.onPickObject.GetComponent<SpriteRenderer>().sprite;
+                            itemCollect.tool2.color = itemCollect.onPickObject.GetComponent<SpriteRenderer>().color;
+                            if (itemCollect.tool2.sprite == itemCollect.tool4.sprite) {
+                                itemCollect.tool4.sprite = null;
+                                itemCollect.tool4.color = Color.clear;
+                            } else if (itemCollect.tool2.sprite == itemCollect.tool5.sprite) {
+                                itemCollect.tool5.sprite = null;
+                                itemCollect.tool5.color = Color.clear;
+                            } else if (itemCollect.tool2.sprite == itemCollect.tool6.sprite) {
+                                itemCollect.tool6.sprite = null;
+                                itemCollect.tool6.color = Color.clear;
+                            }
+                        }
+                        else
+                        {
+                            itemCollect.tool3.sprite = itemCollect.onPickObject.GetComponent<SpriteRenderer>().sprite;
+                            itemCollect.tool3.color = itemCollect.onPickObject.GetComponent<SpriteRenderer>().color;
+                            if (itemCollect.tool3.sprite == itemCollect.tool4.sprite) {
+                                itemCollect.tool4.sprite = null;
+                                itemCollect.tool4.color = Color.clear;
+                            } else if (itemCollect.tool3.sprite == itemCollect.tool5.sprite) {
+                                itemCollect.tool5.sprite = null;
+                                itemCollect.tool5.color = Color.clear;
+                            } else if (itemCollect.tool3.sprite == itemCollect.tool6.sprite) {
+                                itemCollect.tool6.sprite = null;
+                                itemCollect.tool6.color = Color.clear;
+                            }
                         }
                     }
                     else
                     {
-                        itemCollect.tool3.sprite = itemCollect.onPickObject.GetComponent<SpriteRenderer>().sprite;
-                        itemCollect.tool3.color = itemCollect.onPickObject.GetComponent<SpriteRenderer>().color;
-                        if (itemCollect.tool3.sprite == itemCollect.tool4.sprite) {
-                            itemCollect.tool4.sprite = null;
-                            itemCollect.tool4.color = Color.clear;
-                        } else if (itemCollect.tool3.sprite == itemCollect.tool5.sprite) {
-                            itemCollect.tool5.sprite = null;
-                            itemCollect.tool5.color = Color.clear;
-                        } else if (itemCollect.tool3.sprite == itemCollect.tool6.sprite) {
-                            itemCollect.tool6.sprite = null;
-                            itemCollect.tool6.color = Color.clear;
-                        }
+                        Debug.Log("Items Are Full");
                     }
                 }
-                else
+                else if(hitInfo.transform.tag == "Enemy")
                 {
-                    Debug.Log("Items Are Full");
+                    Debug.Log("Hit Enemy");
+                    Destroy(item);
                 }
+            }else{
+                lineRenderer.SetPosition(0, firePoint.position);
+                lineRenderer.SetPosition(1, mousePos*100); 
             }
-            else if(hitInfo.transform.tag == "Enemy")
-            {
-                Debug.Log("Hit Enemy");
-                Destroy(item);
-            }
-        }
-        else
-        {
-            lineRenderer.SetPosition(0, firePoint.position);
-            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);           
+
         }
 
         lineRenderer.enabled = true;
