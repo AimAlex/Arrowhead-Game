acf8304f9ddddfd1e4fa442b2531ce79768da8e1
chrisliu <sduliushunqi@gmail.com>
2022-10-18 18:24:50 -0700

    add cinemacamera pkg

--- Assets/Scripts/Player/SideScrolling.cs
+++ Assets/Scripts/Player/SideScrolling.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class SideScrolling : MonoBehaviour
 {
     public Transform tourCamera;
@@ -21,15 +20,18 @@ public class SideScrolling : MonoBehaviour
         player = GameObject.Find("Player").transform;
         startPosition = transform.position - player.position;
         mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
-        List<Vector3> tourpoints = new List<Vector3>();
-        tourpoints.Add(new Vector3(transform.position.x + 200, transform.position.y + 200, 0));
-        tourpoints.Add(new Vector3(transform.position.x + 400, transform.position.y + 400, 0));
-        tourpoints.Add(new Vector3(transform.position.x + 800, transform.position.y + 800, 0));
-        tourpoints.Add(new Vector3(transform.position.x + 1000, transform.position.y + 1000, 0));
-        tourpoints.Add(new Vector3(transform.position.x + 2000, transform.position.y + 2000, 0));
-        tourpoints.Add(new Vector3(transform.position.x, transform.position.y, 0));
-        // CameraTour(tourpoints);
         // if (tourCamera == null) tourCamera = gameObject.transform;
+    }
+    private void Start(){
+        List<Vector3> tourpoints = new List<Vector3>();
+        Debug.Log("start: " + transform.position);
+        tourpoints.Add(new Vector3(transform.position.x + 200, transform.position.y + 200, transform.position.z));
+        tourpoints.Add(new Vector3(transform.position.x + 400, transform.position.y + 400, transform.position.z));
+        tourpoints.Add(new Vector3(transform.position.x + 800, transform.position.y + 800, transform.position.z));
+        tourpoints.Add(new Vector3(transform.position.x + 1000, transform.position.y + 1000, transform.position.z));
+        tourpoints.Add(new Vector3(transform.position.x + 2000, transform.position.y + 2000, transform.position.z));
+        tourpoints.Add(new Vector3(transform.position.x, transform.position.y, transform.position.z));
+        CameraTour(tourpoints);
     }
     private void Update()
     {
@@ -42,9 +44,6 @@ public class SideScrolling : MonoBehaviour
             mainCamera.orthographicSize = originalSize;
         }
         // GetDirection();
-        // direction.x *= 2;
-        // direction.y *= 2;
-        // transform.Translate(direction * Time.deltaTime, Space.World);
     }
     private void LateUpdate()
     {
@@ -64,39 +63,44 @@ public class SideScrolling : MonoBehaviour
             transform.position = cameraPosition;
         }
     }
-    private void GetDirection()
+    private void GetDirection(Vector3 direction)
     {
-        speedForward = Vector3.zero;
-        speedBack = Vector3.zero;
-        speedLeft = Vector3.zero;
-        speedRight = Vector3.zero;
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.B) || Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E)){
-            camaraMove = false;
-        }
-        if (Input.GetKey(KeyCode.I)){
-            speedForward = transform.up;
-            camaraMove = true;
-        }
-        if (Input.GetKey(KeyCode.K)){
-            speedBack = -transform.up;
-            camaraMove = true;
-        }
-        if (Input.GetKey(KeyCode.J)){
-            speedLeft = -transform.right;
-            camaraMove = true;
-        }
-        if (Input.GetKey(KeyCode.L)){
-            speedRight = transform.right;
-            camaraMove = true;
-        }
-        direction = speedForward + speedBack + speedLeft + speedRight;
+        
+        // speedForward = Vector3.zero;
+        // speedBack = Vector3.zero;
+        // speedLeft = Vector3.zero;
+        // speedRight = Vector3.zero;
+        // if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.B) || Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E)){
+        //     camaraMove = false;
+        // }
+        // if (Input.GetKey(KeyCode.I)){
+        //     speedForward = transform.up;
+        //     camaraMove = true;
+        // }
+        // if (Input.GetKey(KeyCode.K)){
+        //     speedBack = -transform.up;
+        //     camaraMove = true;
+        // }
+        // if (Input.GetKey(KeyCode.J)){
+        //     speedLeft = -transform.right;
+        //     camaraMove = true;
+        // }
+        // if (Input.GetKey(KeyCode.L)){
+        //     speedRight = transform.right;
+        //     camaraMove = true;
+        // }
+        // direction = speedForward + speedBack + speedLeft + speedRight;
+        direction.x *= 2;
+        direction.y *= 2;
+        transform.Translate(direction * Time.deltaTime, Space.World);
+
     }
     public void CameraTour(List<Vector3> tourPoints)
     {
         camaraMove = true;
         for (int i = 0; i < tourPoints.Count; i++)
         {
-            transform.position = Vector3.Lerp(transform.position, tourPoints[i], 10000 * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, tourPoints[i], Time.deltaTime);
             Debug.Log(transform.position);
         }
         camaraMove = false;
