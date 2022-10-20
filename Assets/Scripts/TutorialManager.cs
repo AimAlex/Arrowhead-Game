a4abd0a9af2683216a19b16e420fdb65eb783275
liuzyi219 <zliu2803@gmail.com>
2022-10-20 00:16:17 -0700

    tutorial4

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -123,6 +123,10 @@ public class TutorialManager : MonoBehaviour
             {
                 BombTutorial();
             }
+            else if (popUpIndex == 2)
+            {
+                PauseUntilPress();
+            }
         }
     }
 
