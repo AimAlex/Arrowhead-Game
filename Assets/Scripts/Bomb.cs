f4c4bba86f2d1fd532c6d4acf98300c8c4d72516
Cypher <46045772+leafCyhvss@users.noreply.github.com>
2022-10-20 17:38:06 -0700

    update level4 fix bugs

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -10,7 +10,7 @@ public class Bomb: MonoBehaviour
 {
 
     public float waitTime;
-    public float radius=4;
+    public float radius = 1;
     private Collider2D Coll;
     private Rigidbody2D Rb;
     public GameObject bomb;
