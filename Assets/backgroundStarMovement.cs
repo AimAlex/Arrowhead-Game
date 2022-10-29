6bc40a0043b1126ee75e9a6a495f3ed52368d4f7
Xintongch <50897973+xintongch@users.noreply.github.com>
2022-10-29 10:16:53 -0700

    update level3 UI

--- Assets/backgroundStarMovement.cs
+++ Assets/backgroundStarMovement.cs
@@ -7,7 +7,6 @@ public class backgroundStarMovement : MonoBehaviour
     // [SerializeField] private float verticalY=1, horizontalX=100;
     // [SerializeField] private float speed = 0;
     [SerializeField] public float speedX = 0.35f;
-    // [SerializeField] public float speedY = 0.0035f;
     [SerializeField] public float speedY = 0.0035f;
     private Vector2 startPosition=new Vector2(-100,10);
     private Vector2 endPosition=new Vector2(120,-30);
@@ -22,8 +21,6 @@ public class backgroundStarMovement : MonoBehaviour
         verticalDir = -1;
         horizontalDir = 1;
         speedY = speedX/100;
-        // verticalDistance = verticalY;
-        // startPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -31,19 +28,9 @@ public class backgroundStarMovement : MonoBehaviour
     {
         transform.Translate(verticalDir * speedY * Time.deltaTime * Vector3.up);
         transform.Translate(horizontalDir * speedX * Time.deltaTime * Vector3.right);
-        Debug.Log("y="+transform.position.y.ToString());
-        Debug.Log("x="+transform.position.x.ToString());
-        Debug.Log("starty="+startPosition.y.ToString());
-        Debug.Log("startx="+startPosition.x.ToString());
         if (transform.position.x > endPosition.x) {
             Debug.Log("reach vertical distance goal");
             this.transform.position=startPosition;
         }
-        // if (verticalDistance > 0 && startPosition.y-transform.position.y > verticalDistance) {
-        //     Debug.Log("reach vertical distance goal");
-        //     this.transform.position=startPosition;
-        //     verticalY=200;
-        //     horizontalX=
-        // }
     }
 }
