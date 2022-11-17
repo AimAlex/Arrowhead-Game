ba18afeacd67eff9d4d9bef2595c84bb0cea6ed0
Qizhuo Shen <sqz0914@gmail.com>
2022-11-16 18:50:48 -0800

    Update Boss.cs

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -5,27 +5,13 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float vertical, horizontal;
-    // [SerializeField] private float speed = 0;
+    [SerializeField] private float horizontal;
     [SerializeField] public float speed = 0;
     private Vector2 startPosition;
-    private int verticalDir = 0;
     private int horizontalDir = 0;
-    private float verticalDistance = 0;
     private float horizontalDistance = 0;
     void Start()
     {
-        if (vertical > 0)
-        {
-            verticalDir = 1;
-            verticalDistance = vertical;
-        }
-        else if (vertical < 0)
-        {
-            verticalDir = -1;
-            verticalDistance = -vertical;
-        }
-
         if (horizontal > 0)
         {
             horizontalDir = 1;
@@ -42,13 +28,6 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (verticalDistance > 0 && transform.position.y - startPosition.y > verticalDistance) {
-            verticalDir = -1;
-        }
-        else if (verticalDistance > 0 && startPosition.y - transform.position.y > verticalDistance) {
-            verticalDir = 1;
-        }
-
         if (horizontalDistance > 0 && transform.position.x - startPosition.x > horizontalDistance)
         {
             horizontalDir = -1;
@@ -57,8 +36,7 @@ public class Boss : MonoBehaviour
         {
             horizontalDir = 1;
         }
-
-        transform.Translate(verticalDir * speed * Time.deltaTime * Vector3.up);
+        
         transform.Translate(horizontalDir * speed * Time.deltaTime * Vector3.right);
     }
 }
