d1aea6407d29a7382ad7b4ace1f0a5b48b414b8f
Jiangbei Li <aimalex918@gmail.com>
2022-11-17 17:44:11 -0800

    update 1-0 and add O to open instruction

---
+++ Assets/Instruction_manager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instruction_manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public GameObject instructionObj;
+    private bool instrShow;
+    void Start()
+    {
+        instrShow = false;
+        instructionObj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            setIntr();
+        }
+    }
+
+    public void setIntr()
+    {
+        if (instructionObj.activeInHierarchy)
+        {
+            instructionObj.SetActive(false);
+            Time.timeScale = 1;
+        }
+        else if (Time.timeScale != 0)
+        {
+            instructionObj.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    
+}
