43b2c78ecee0ed51b2979d9547550d48bf208db1
liuzyi219 <liuziyi219@gmail.com>
2022-11-19 15:12:29 -0800

    animation

--- Assets/Scripts/BombAnimation.cs
+++ Assets/Scripts/BombAnimation.cs
@@ -10,7 +10,7 @@ public class BombAnimation : MonoBehaviour
     public bool second1 = false;
 
     public bool second2 = false;
-    public bool idle = true;
+    public bool idle = false;
 
     public static Animator anim;
     // Start is called before the first frame update
@@ -22,21 +22,34 @@ public class BombAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (isExplode)
         {
+            Debug.Log("explode");
             anim.SetBool("explode", true);
+            anim.SetBool("bomb2", false);
+            isExplode = false;
+            
         }
         else if (second1)
         {
+            Debug.Log("bomb1");
             anim.SetBool("bomb1", true);
+            anim.SetBool("Idle", false);
+            second1 = false;
         }
         else if (second2)
         {
+            Debug.Log("bomb2");
             anim.SetBool("bomb2", true);
+            anim.SetBool("bomb1", false);
+            second2 = false;
         }
         else if (idle)
         {
             anim.SetBool("Idle", true);
+            idle = false;
+            Debug.Log("Idle");
         }
     }
 }
